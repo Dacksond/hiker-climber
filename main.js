fdb7b49beff6869854c1c21bc76aba84cae29999
Jason Marlow <jasonmarlow@gmail.com>
2023-11-16 20:05:36 -0500

    added npm and server code

---
+++ main.js
@@ -0,0 +1,16 @@
+// const http = require('http')
+// const fs = require('fs')
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'content-type': 'text/html' })
+//   fs.createReadStream('index.html').pipe(res)
+// })
+
+// server.listen(process.env.PORT || 3000)
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+connect()
+  .use(serveStatic(__dirname))
+  .listen(3000, () => console.log('Server running on 3000...'));
